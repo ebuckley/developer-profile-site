80664578286b783466e27f9f30c594df2e8a3413
Ersin Buckley <ersin.buckley@emulex.com>
2015-09-25 16:00:29 +1200

    implement some tests

--- tests.js
+++ tests.js
@@ -1,13 +1,33 @@
 describe('When using the devloper profile app', function  () {
-	var CVData;
 	beforeEach(function () {
 		angular.mock.module('eb.cv');
-		inject(function  (_CVData_) {
-			CVData = CVData;		
-		});
 	});
 
 	it('should have loaded the app', function  () {
 		expect(angular).not.toBeUndefined();
+	});
+
+	describe('when using CVData', function  () {
+		var CVData;
+		beforeEach(inject(function (_CVData_) {
+			CVData = _CVData_;
+		}));
+		it('should have a name', function  () {
+			expect(CVData.name).not.toBeUndefined();
+		});
+		it('should have an array of organizations', function  () {
+			expect(CVData.organizations.length).not.toBeUndefined();
+		});
+
+
+		describe('when looking at the organizations', function  () {
+			it('should have valid items', function  () {
+				CVData.organizations.forEach(function  (item, index) {
+					//it should have a name
+					expect(item.name).not.toBeUndefined();
+				});
+			});
+		});
+		
 	});
 });
